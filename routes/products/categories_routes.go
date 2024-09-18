da179979e65b448093bf44a7ae33ad4717c4858f
Jefferson Rivera <jefersonpatino@yahoo.es>
2024-09-18 15:55:25 -0500

    Refactorizar rutas de categorías de productos

--- routes/products/categories_routes.go
+++ routes/products/categories_routes.go
@@ -6,18 +6,9 @@ import (
 )
 
 func CategoryRoutes(r *gin.Engine) {
-	// Create category
 	r.POST("/categories", products.CreateCategory)
-
-	// Get all categories
 	r.GET("/categories", products.GetCategories)
-
-	// Get category by ID
 	r.GET("/categories/:id", products.GetCategory)
-
-	// Update category
 	r.PUT("/categories/:id", products.UpdateCategory)
-
-	// Delete category
 	r.DELETE("/categories/:id", products.DeleteCategory)
 }
